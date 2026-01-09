b810dbc1ace7a4a16590cd99b8330db4482c5a4d
Lucas <lubapisilva@gmail.com>
2026-01-09 14:27:47 -0300

    Revert "fix: resolve erro em deploy"
    This reverts commit 3d604788b62153af4500f9431b8aa3d12f62ab11.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        if (App::environment('production')) {
-            return;
-        }
-
         $this->call([
             OperatingHoursSeeder::class,
             UserSeeder::class,
